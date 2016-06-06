16cc469b595442764d9b41883137dd4e29e51f3a
cameronmathies <cmathies@mr.preble.com>
2016-06-06 13:06:05 -0400

    Player and food

---
+++ Preble.IO Project/Assets/Points.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Points : MonoBehaviour
+{
+
+    public int PointsInt;
+    public Text PointsText;
+    public GameObject Bullet;
+    public GameObject cube;
+    void Start()
+    {
+        PointsInt = 0;
+    }
+
+    void OnTriggerEnter()
+    {
+
+        if (Bullet.tag == "Food")
+        {
+            PointsInt += 1;
+            StartCoroutine(Killers());
+        }
+    }
+    void Update()
+    {
+        PointsText.text = "Score  " + PointsInt;
+    }
+
+    IEnumerator Killers()
+    {
+        Destroy(Bullet);
+        Destroy(cube);
+        yield return new WaitForSeconds(1);
+    }
+}
