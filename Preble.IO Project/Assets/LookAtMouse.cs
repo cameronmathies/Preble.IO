b0543b79c1033cd55fefda5dd03eae9702817e03
cameronmathies <cmathies@mr.preble.com>
2016-06-06 12:23:50 -0400

    Player Done

---
+++ Preble.IO Project/Assets/LookAtMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour {
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+}
